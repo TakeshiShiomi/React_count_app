b19ef2ffd9a45d53a4beaea3839b662895be974b
TakeshiShiomi <takeyan403@gmail.com>
2022-06-20 23:17:14 +0900

    ボタンとカウントの表示

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,26 @@
+import { count } from 'console';
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            count: 0
+        };
+    }
+
+    increment = () => {
+        this.setState({
+          count: this.state.count + 1
+        });
+    };
+
+    decrement = () => {
+        this.setState({
+          count: this.state.count - 1
+        });
+    };
+
     render(){
       return(
         <div>
